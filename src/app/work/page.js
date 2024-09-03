4b5c5e8074e212fb8f8fc1756fedaa623d36d8df
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-03 14:01:08 +0700

    update project

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -1,5 +1,22 @@
 export default function work(){
     return (
-<>work</>
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project UI UX Design</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">pembuatan design dari aplikasi coffee.</p>
+        <a href="https://www.figma.com/design/3FXuPfNfupIJF19WdYN0vi/aplikasi-coffee?node-id=0-1&t=PsyvgYUiFRKvCpfL-1" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            liat Project
+             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+</div>
+
     );
 }
