85263544f6a9bebb172c3dffa316f8e9a5e6592a
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-08 16:20:41 +0700

    updatte work

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -4,9 +4,6 @@ export default function work(){
     return (
         <div class="flex flex-wrap justify-center gap-4">
             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img class="rounded-t-lg" src="#" alt=""/>
-                </a>
                 <div class="p-5">
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project UI UX Design</h5>
@@ -21,9 +18,6 @@ export default function work(){
                 </div>
             </div>
             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="">
-                    <img class="rounded-t-lg" src="#" alt="" />
-                </a>
                 <div class="p-5">
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project UI UX Design</h5>
@@ -38,5 +32,6 @@ export default function work(){
                 </div>
             </div>
         </div>
+        
     );
 }
