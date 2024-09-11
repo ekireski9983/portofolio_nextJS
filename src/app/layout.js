01e732e60cb91fccea4d5fee6b5c8e90d51b710e
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-11 18:31:37 +0700

    update layout dan file

--- src/app/layout.js
+++ src/app/layout.js
@@ -78,7 +78,7 @@ export default function RootLayout({ children }) {
                         About
                       </a>
                       <a
-                        href="/work"
+                        href="/project"
                         className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                       >
                         Projects
@@ -114,7 +114,7 @@ export default function RootLayout({ children }) {
               <a href="/" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</a>
               <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
               <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">contact</a>
-              <a href="/work" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+              <a href="/project" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
               </div>
             </div>
           </nav>
