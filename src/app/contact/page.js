78ab36e4a284d931b9a962655119340afad49739
ekireski9983 <rezkyjayadis@gmail.com>
2024-09-11 20:27:34 +0700

    menambahkan textarea

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -47,7 +47,9 @@ export default function form(){
             </label>
             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210"></input>
           </div>
-        </div>
+          <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">saran</label>
+          <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="tuliskan maksimal 50-100 karakter"></textarea>
+         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         konfirmasi
         </button>
